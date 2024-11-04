f8d74e18f66a9f32137e039975eaaf770811e958
blah <blah>
2024-11-04 04:09:24 -0500

    get simple test request working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,15 @@ import { createClient } from "@connectrpc/connect";
 import { createConnectTransport } from "@connectrpc/connect-web";
 
 import { RestaurantService } from './gen/restaurant/v1/restaurant_connect';
-import { GetRestaurantsRequest } from './gen/restaurant/v1/restaurant_pb';
+import { GetRestaurantsRequest, RestaurantInfo } from './gen/restaurant/v1/restaurant_pb';
 
 
 
 function App() {
   const [count, setCount] = useState(0)
 
-  // const transport = createConnectTransport({
-  //   baseUrl: "https://demo.connectrpc.com",
-  // });
+  const [restaurants, setRestaurants] = useState<RestaurantInfo[]>([]);
+  const [error, setError] = useState<string>("");
 
   const client = createClient(
     RestaurantService,
@@ -20,6 +19,26 @@ function App() {
       baseUrl: "http://localhost:8080/",
     }),
   );
+
+  const getRestaurants = async () => {
+    const request = new GetRestaurantsRequest({
+      zipcodes: ["10001"],  
+      cuisines: ["Italian"], 
+      healthRating: "A",    
+    });
+
+    try {
+      const response = await client.getRestaurants(request);
+      setRestaurants(response.restaurants);
+    } catch (error) {
+      setError("Error fetching restaurants: " + error);
+      console.error("Error getting restaurants:", error);
+    }
+  };
+
+  useEffect(() => {
+    getRestaurants()
+  }, []); 
 
   return (
    <div>
